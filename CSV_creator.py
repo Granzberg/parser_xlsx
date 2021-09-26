296bcdb2a63c7b05200cb2d02cafb1ae7ddaad1a
Granzberg <gering225@gmail.com>
2021-09-26 10:16:44 +0300

    update 001

--- CSV_creator.py
+++ CSV_creator.py
@@ -1,63 +1,90 @@
 import pandas as pd
 data = {}
 data_choice = ['patronymic', 'spec', 'corp']
+fn = './fuaid.xlsx'
 
-
-xlsx = pd.read_excel('./fuaid.xlsx',sheet_name='Sheet1',usecols=['patronymic','spec','corp'])
+xlsx = pd.read_excel(fn, 0, usecols=data_choice, index_col=None)
 data.update(xlsx)
 
-# создание списка на выбор 'data_choice'(0 - имена, 1 - специальности, 2 - корп.почта)
+
 def create_list_of_names(list_names):
-    names = set()
-    name = []
-    names.update(data[data_choice[0]])
-    names = list(names)
-    length = len(names)
-    for i in range(length):
-        h = names[i].split()
-        name.append(h)
-    return name
+    # создание списка на выбор 'data_choice'(0 - имена, 1 - специальности, 2 - корп.почта)
+    names = {}
+    data_clean = []
+    names.update(list_names[data_choice[0]])
+    n = []
+    for item in names.values():
+        n.append(item)
+    spec = {}
+    spec.update(list_names[data_choice[1]])
+    sp = []
+    for item in spec.values():
+        sp.append(item)
+    corp = {}
+    corp.update(list_names[data_choice[2]])
+    e = []
+    for item in corp.values():
+        e.append(item)
+    data_clean.append(n)
+    data_clean.append(sp)
+    data_clean.append(e)
+    #print(data_clean)
+    return data_clean
 
-# создание списка имен
-def names_str(name_s):
-    for name_l in list_of_names:
-        name_1 = str(name_l[0])
-        return name_1
 
-
-# создание списка фамилий
 def surname_str(name_s):
-    for name_l in list_of_names:
-        surname = str(name_l[1])
-        return surname
+    # создание списка фамилий
+    surname = []
+    for u in name_s[0]:
+        surname.append(u)
+    surname = [[item] for item in surname]
+    print(surname)
+    return surname
 
-# создание списка почты(в процессе)
-def corporate_emails(list_names):
-    emails = set()
-    corp_email = []
-    emails.update(data[data_choice[2]])
-    emails = list(emails)
-    length = len(emails)
-    for i in range(length):
-        h = emails[i].split()
-        corp_email.append(h)
 
-    return corp_email
+def names_str(name_s):
+    # создание списка имен
+    names = []
+    for i in name_s:
+        names.append(i[1:2])
+    return names
 
-def cor_email(corp_email):
-    for em in corp_email:
-        email = str(em)
-        return email
 
-#choice = int(input('Какие данные нужны?(0, 1, 2): '))
-#choice = 2
+# def corporate_emails(list_names):
+#     # создание списка почты
+#     emails = []
+#     corp_email = []
+#     emails.append(list_names[data_choice[2]])
+#     emails = list(emails)
+#     length = len(emails)
+#     for i in range(length):
+#         corp_email.append(emails[i])
+#     return corp_email
+
+
+def cor_email(corp_email1):
+    # создание корп. почты
+    emails = []
+    for em in corp_email1:
+        emails.append(em)
+    return emails
+
+# choice = int(input('Какие данные нужны?(0, 1, 2): '))
+# choice = 2
+
+
 list_of_names = create_list_of_names(xlsx)
+#list_of_corp_email = corporate_emails(xlsx)
+s = surname_str(list_of_names)
+#print(s)
+#print(list_of_names)
+#print(list_of_corp_email)
 
-list_of_corp_email = corporate_emails(xlsx)
+
 
 # write to Excel
-df = pd.DataFrame({'Name':[names_str(list_of_names)],
-                   'Surname':[surname_str(list_of_names)],
-                   'Corp_emails':[cor_email(list_of_corp_email)]})
-with pd.ExcelWriter('./test.xlsx') as writer:
-    df.to_excel(writer, sheet_name="Sheet1")
+# df = pd.DataFrame({'Name': names_str(list_of_names),
+#                    'Surname': surname_str(list_of_names),
+#                    'Corp_emails': cor_email(list_of_corp_email)})
+# with pd.ExcelWriter('./test.xlsx') as writer:
+#     df.to_excel(writer, sheet_name="Sheet1")
