2c3d262ef3dec869816daf4a2fc2fa8007f22a04
Granzberg <gering225@gmail.com>
2021-10-08 10:34:43 +0300

    update 007

--- translation_words.py
+++ translation_words.py
@@ -12,26 +12,45 @@ alternate_letters = {'є': 'ie', 'ї': 'i', 'й': 'i', 'ю': 'iu', 'я': 'ia'}
 
 
 def create_list_of_word(list_names):
+    # создание списка имен
     names = {}
-    data_clean = []
     names.update(list_names[words[0]])
     n = []
     for item in names.values():
         n.append(item)
-
+    # print(type(n))
+    # print(n)
     return n
 
 
-def translation_of_word(data_clean):
-    for i in data_clean:
-        length = len(i)
-        for l in range(length):
-            pass
+def splitting_word_into_letters(data_clean):
+    # Раздиление слов на буквы
+    separated_words_into_letters = []
+    translated_word = []
+    for w in data_clean:
+        k = list(w)
+        separated_words_into_letters.append(k)
+
+    return separated_words_into_letters
+
+
+def translation_of_word(list_of_words, dictionary):
+    w = 0
+    words = []
+    for word in list_of_words[w]:
+        for letter in word:
+            words.append(letter.lower())
+    print(words)
+    for k,v in dictionary.items():
+        n = {k: dictionary[k] for k in dictionary.keys() & set(list_of_words)}
+        print(n)
+
+
+
 
 
 xlsx = pd.read_excel(fn, 0, usecols=words, index_col=None)
 data.update(xlsx)
 
-w = create_list_of_word(xlsx)
-
-translation_of_word(w)
+w = splitting_word_into_letters(create_list_of_word(xlsx))
+translation_of_word(w, letters)
