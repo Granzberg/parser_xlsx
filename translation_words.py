a3cef80c72c846b711706ada9dc133a05342af74
Granzberg <gering225@gmail.com>
2021-10-18 18:05:25 +0300

    update 010

--- translation_words.py
+++ translation_words.py
@@ -1,12 +1,13 @@
 import pandas as pd
+
 fn = './test.xlsx'
 words = ['Name', 'Surname']
 data = {}
 
 letters = {'а': 'a', 'б': 'd', 'в': 'v', 'г': 'h', 'ґ': 'g', 'д': 'd', 'е': 'e', 'є': 'ye', 'ж': 'zh', 'з': 'z',
-           'и': 'y','і': 'i', 'ї': 'yi', 'й': 'y', 'к': 'k', 'л': 'l', 'м': 'm', 'н': 'n', 'о': 'o', 'р': 'r',
+           'и': 'y', 'і': 'i', 'ї': 'yi', 'й': 'y', 'к': 'k', 'л': 'l', 'м': 'm', 'н': 'n', 'о': 'o', 'р': 'r',
            'с': 's', 'т': 't', 'у': 'u', 'ф': 'f', 'х': 'kh', 'ч': 'ch', 'ш': 'sh', 'щ': 'shch', 'ю': 'yu', 'я': 'ya',
-           'зг': 'zgh'}
+           '1': 'zgh'}
 
 alternate_letters = {'є': 'ie', 'ї': 'i', 'й': 'i', 'ю': 'iu', 'я': 'ia'}
 
@@ -18,41 +19,53 @@ def create_list_of_word(list_names):
     n = []
     for item in names.values():
         n.append(item)
-    # print(type(n))
-    # print(n)
     return n
 
 
 def splitting_word_into_letters(data_clean):
     # Раздиление слов на буквы
     separated_words_into_letters = []
-    translated_word = []
-    for w in data_clean:
-        k = list(w)
+    f = str(data_clean)
+    # фильтр на комбинацыю символов "зг"
+
+    q = str()
+    for w in f:
+        print(w)
+        if w.find('зг', 0) == -1:
+            k = list(w)
+        else:
+            print(f)
+            q = w.replace('зг', '0')
+            k = list(q)
+            print(q)
         separated_words_into_letters.append(k)
 
     return separated_words_into_letters
 
 
 def transmutation_of_word(list_of_words, list_letters):
+    # Перевод слов по таблице перевода
     translate_words = [[]] * len(list_of_words)
     n = 0
     for words_list in list_of_words:
         k = []
         for letter in words_list:
-
-            if letter.lower() in list_letters:
+            if letter in list_letters:
                 words = list_letters[letter.lower()]
                 k.append(words)
-
-        translate_words[n] = k
+        conk = ''
+        for i in k:
+            conk += i
+        translate_words[n] = [conk.capitalize()]
         n += 1
     return translate_words
-
 
 
 xlsx = pd.read_excel(fn, 0, usecols=words, index_col=None)
 data.update(xlsx)
 
 w = splitting_word_into_letters(create_list_of_word(xlsx))
-print(transmutation_of_word(w, letters))
+#print(w)
+transmutation_of_word(w, letters)
+
+# https://foxford.ru/wiki/informatika/zadachi-poiska-zameny-i-udaleniya-podstroki-v-stroke-v-python
