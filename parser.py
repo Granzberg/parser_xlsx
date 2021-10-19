f0e53d0b39d4c716a45940f45461da4cb74f73d5
Granzberg <gering225@gmail.com>
2021-10-19 15:45:16 +0300

    update 011

--- parser.py
+++ parser.py
@@ -1,56 +1,93 @@
-import requests
-from bs4 import BeautifulSoup
+import pandas as pd
 
-URL = 'https://dmsu.gov.ua/services/transliteration.html'
-BASE_URL = 'https://dmsu.gov.ua'
-HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)'
-                         ' Chrome/92.0.4515.159 Safari/537.36', 'accept': '*/*'}
+fn = './test.xlsx'
+words = ['Name', 'Surname']
+data = {}
 
-def getting_translation(name, surname):
-    headers = {
-        'referer': '{}/services.html'.format(BASE_URL)
-    }
-    payload = {
-        'name': name,
-        'surname': surname,
-        #'argument': translate
-    }
-    r = requests.post(
-        '{}/services/transliteration.html'.format(BASE_URL),
-        data=payload,
-       # headers=headers
-    )
-    r.raise_for_status()
-    return r.text
+letters = {'а': 'a', 'б': 'd', 'в': 'v', 'г': 'h', 'ґ': 'g', 'д': 'd', 'е': 'e', 'є': 'ye', 'ж': 'zh', 'з': 'z',
+           'и': 'y', 'і': 'i', 'ї': 'yi', 'й': 'y', 'к': 'k', 'л': 'l', 'м': 'm', 'н': 'n', 'о': 'o', 'р': 'r',
+           'с': 's', 'т': 't', 'у': 'u', 'ф': 'f', 'х': 'kh', 'ч': 'ch', 'ш': 'sh', 'щ': 'shch', 'ю': 'yu', 'я': 'ya',
+           '0': 'zgh'}
+
+alternate_letters = {'є': 'ie', 'ї': 'i', 'й': 'i', 'ю': 'iu', 'я': 'ia'}
 
 
-def get_html(url, params=None):
-    r = requests.get(url, headers=HEADERS, params=params)
-    return r
+def create_list_of_names(list_names):
+    # создание списка имен
+    names = {}
+    names.update(list_names[words[0]])
+    n = []
+    for item in names.values():
+        n.append(item)
+    return n
 
 
-def get_content(html):
-    soup = BeautifulSoup(html, 'html.parser')
-    items = soup.find_all('div', class_='card-container')
-
-    cars = []
-    for item in items:
-        cars.append({
-            'title': item.find('div', class_='bold-field').get_text(strip=True),
-            #'title2': item.find('div', class_='bold-field').get_text(),
-        })
-    print(cars)
+def create_list_of_surname(list_names):
+    # создание списка имен
+    names = {}
+    names.update(list_names[words[1]])
+    n = []
+    for item in names.values():
+        n.append(item)
+    return n
 
 
-def parse():
-    html = get_html(URL)
-    if html.status_code == 200:
-        get_content(html.text)
-        #print('ok')
-    else:
-        print('Error')
+def splitting_word_into_letters(data_clean):
+    # Раздиление слов на буквы
+    separated_words_into_letters = []
+    # фильтр на комбинацыю символов "зг"
+    k = ''
+    for w in data_clean:
+        if w.find('зг', 0) == -1:
+            k = list(w)
+            separated_words_into_letters.append(k)
+        else:
+            q = w.replace('зг', '0')
+            k = list(q)
+            separated_words_into_letters.append(k)
+
+    return separated_words_into_letters
 
 
-getting_translation(surname='Шишов', name='я')
+def transmutation_of_word(list_of_words, list_letters, second_filter):
+    # Перевод слов по таблице перевода
+    translate_words = [[]] * len(list_of_words)
+    n = 0
+    # Работает не правильно ...
+    # походу требуется разбить на болие мелкие функции и использввать их как методы в нутри этой
+    for words_list in list_of_words:
+        h = []
+        test = []
+        k = []
+        for w in words_list:
+            if w in second_filter:
+                h = second_filter[w]
+                test.append(h)
+            else:
+                test.append(w)
+                for letter in test:
+                    if letter.lower() in list_letters:
+                        words = list_letters[letter.lower()]
+                        k.append(words)
+        conk = ''
+        for i in k:
+            conk += i
+        translate_words[n] = [conk.capitalize()]
+        n += 1
+    print(translate_words)
+    # return translate_words
 
-parse()
+
+
+xlsx = pd.read_excel(fn, 0, usecols=words, index_col=None)
+data.update(xlsx)
+
+#rint(xlsx)
+n = splitting_word_into_letters(create_list_of_names(xlsx))
+transmutation_of_word(n, letters, alternate_letters)
+#
+# s = splitting_word_into_letters(create_list_of_surname(xlsx))
+# surname = transmutation_of_word(s, letters, alternate_letters)
+
+# print('*************************************')
+# print(surname)
--- translation_words.py
+++ translation_words.py
@@ -7,12 +7,12 @@ data = {}
 letters = {'а': 'a', 'б': 'd', 'в': 'v', 'г': 'h', 'ґ': 'g', 'д': 'd', 'е': 'e', 'є': 'ye', 'ж': 'zh', 'з': 'z',
            'и': 'y', 'і': 'i', 'ї': 'yi', 'й': 'y', 'к': 'k', 'л': 'l', 'м': 'm', 'н': 'n', 'о': 'o', 'р': 'r',
            'с': 's', 'т': 't', 'у': 'u', 'ф': 'f', 'х': 'kh', 'ч': 'ch', 'ш': 'sh', 'щ': 'shch', 'ю': 'yu', 'я': 'ya',
-           '1': 'zgh'}
+           '0': 'zgh'}
 
 alternate_letters = {'є': 'ie', 'ї': 'i', 'й': 'i', 'ю': 'iu', 'я': 'ia'}
 
 
-def create_list_of_word(list_names):
+def create_list_of_names(list_names):
     # создание списка имен
     names = {}
     names.update(list_names[words[0]])
@@ -22,24 +22,29 @@ def create_list_of_word(list_names):
     return n
 
 
+def create_list_of_surname(list_names):
+    # создание списка имен
+    names = {}
+    names.update(list_names[words[1]])
+    n = []
+    for item in names.values():
+        n.append(item)
+    return n
+
+
 def splitting_word_into_letters(data_clean):
     # Раздиление слов на буквы
     separated_words_into_letters = []
-    f = str(data_clean)
     # фильтр на комбинацыю символов "зг"
-
-    q = str()
-    for w in f:
-        print(w)
+    k = ''
+    for w in data_clean:
         if w.find('зг', 0) == -1:
             k = list(w)
+            separated_words_into_letters.append(k)
         else:
-            print(f)
             q = w.replace('зг', '0')
             k = list(q)
-            print(q)
-        separated_words_into_letters.append(k)
-
+            separated_words_into_letters.append(k)
     return separated_words_into_letters
 
 
@@ -50,7 +55,7 @@ def transmutation_of_word(list_of_words, list_letters):
     for words_list in list_of_words:
         k = []
         for letter in words_list:
-            if letter in list_letters:
+            if letter.lower() in list_letters:
                 words = list_letters[letter.lower()]
                 k.append(words)
         conk = ''
@@ -64,8 +69,13 @@ def transmutation_of_word(list_of_words, list_letters):
 xlsx = pd.read_excel(fn, 0, usecols=words, index_col=None)
 data.update(xlsx)
 
-w = splitting_word_into_letters(create_list_of_word(xlsx))
-#print(w)
-transmutation_of_word(w, letters)
+n = splitting_word_into_letters(create_list_of_names(xlsx))
+name = transmutation_of_word(n, letters)
+
+s = splitting_word_into_letters(create_list_of_surname(xlsx))
+surname = transmutation_of_word(s, letters)
+print(name)
+print('*************************************')
+print(surname)
 
 # https://foxford.ru/wiki/informatika/zadachi-poiska-zameny-i-udaleniya-podstroki-v-stroke-v-python
