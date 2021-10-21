aae5c67f4d09d4046788a7645d38412a355e6687
Granzberg <gering225@gmail.com>
2021-10-21 16:24:16 +0300

    upadate 015

--- create_corporate_email.py
+++ create_corporate_email.py
@@ -1,10 +1,37 @@
 import pandas as pd
 
-fn = './test.xlsx'
-words = ['Name', 'Surname']
+fn = './translation_names.xlsx'
+words = ['Surname_TW', 'Specialty number']
 data = {}
 
+
+def list_of_surnames(raw_data):
+    list_surnames = []
+    for surnames in raw_data[words[0]]:
+        list_surnames.append(surnames)
+    return list_surnames
+
+def list_of_specialty_numbers(raw_data):
+    list_specialty_numbers = []
+    for specialty_numbers in raw_data[words[1]]:
+        list_specialty_numbers.append(specialty_numbers)
+    return list_specialty_numbers
+
+
+def create_emails(surnames, emails):
+    test = {}
+    email = []
+    email.append(emails)
+    email.append(surnames)
+    for i in email:
+        test.update(i)
+    print(test)
 
 
 xlsx = pd.read_excel(fn, 0, usecols=words, index_col=None)
 data.update(xlsx)
+
+surname = list_of_surnames(data)
+numbers = list_of_specialty_numbers(data)
+create_emails(surname, numbers)
+#print(surname)
