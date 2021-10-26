9905c0fc05492aba718d077730585943eec00ac4
Granzberg <74976017+Granzberg@users.noreply.github.com>
2021-10-26 16:32:15 +0300

    Update sort_data_analisys.py

--- sort_data_analisys.py
+++ sort_data_analisys.py
@@ -5,8 +5,15 @@ data_choice = ['First Name [Required]',	'Last Name [Required]',	'Email Address [
 fn = './read_data.xlsx'
 
 
+def sort_data(data_list):
+    data = []
+    for i in data_list[data_choice[0]]:
+        print(i)
+
+
+
 # Нужно сделать итерацию и сравнение каждой строки со списком .... и вывода похожих строк в отдельный файл..
 xlsx = pd.read_excel(fn, 0, usecols=data_choice, index_col=None)
 data.update(xlsx)
 
-print(data)
+sort_data(data)
